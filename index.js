10c8d3465f5079ff5bce910e4ada300f2cf0b754
‘asel98x’ <vinjith98@gmail.com>
2022-12-07 16:50:30 +0530

    special price list added

--- index.js
+++ index.js
@@ -20,5 +20,15 @@ $(document).ready(function(){
             }
         }
     });
+
+    var $grid = $(".grid").isotope({
+        itemSelector : '.grid-item',
+        layoutMode : 'fitRows'
+    });
+
+    $(".button-group").on("click", "button", function(){
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue});
+    })
 });
 
