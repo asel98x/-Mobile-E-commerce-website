48eb277a9605e2bb5ff78d8d9e1f9abe979239b2
‘asel98x’ <vinjith98@gmail.com>
2022-12-15 21:30:28 +0530

    db new phone section added

--- Templete/_new-phones.php
+++ Templete/_new-phones.php
@@ -6,125 +6,32 @@
         <div class="owl-carousel owl-theme">
 
             <!--products-->
-            <div class="item  py-2 bg-light">
-                <div class="product font-rale">
-                    <a href="#"><img src="./assests/products/1.png" alt="product1" class="img-fluid"></a>
-                    <div class="text-centure">
-                        <h6>Samsung Galaxy Tab</h6>
-                        <div class="rating text-warning font-size-12">
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="far fa-star"></i></span>
+            <?php
+            foreach ($product_shuffle as $item){?>
+                <!--products-->
+                <div class="item  py-2 bg-light">
+                    <div class="product font-rale">
+                        <a href="#"><img src="<?php echo $item['item_image']??"./assests/products/1.png";?>" alt="product1" class="img-fluid"></a>
+                        <div class="text-centure">
+                            <h6><?php echo $item['item_name']??"Unknown";?></h6>
+                            <div class="rating text-warning font-size-12">
+                                <span><i class="fas fa-star"></i></span>
+                                <span><i class="fas fa-star"></i></span>
+                                <span><i class="fas fa-star"></i></span>
+                                <span><i class="fas fa-star"></i></span>
+                                <span><i class="far fa-star"></i></span>
+                            </div>
+                            <div class="price py-2">
+                                <span>$<?php echo $item['item_price']??'0';?></span>
+                            </div>
+                            <button type="submit" class="btn btn-warning font-size-12">Add to cart</button>
                         </div>
-                        <div class="price py-2">
-                            <span>$152</span>
-                        </div>
-                        <button type="submit" class="btn btn-warning font-size-12">Add to cart</button>
                     </div>
                 </div>
-            </div>
-
-            <div class="item  py-2 bg-light">
-                <div class="product font-rale">
-                    <a href="#"><img src="./assests/products/2.png" alt="product1" class="img-fluid"></a>
-                    <div class="text-centure">
-                        <h6>Redmi note 7</h6>
-                        <div class="rating text-warning font-size-12">
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="far fa-star"></i></span>
-                        </div>
-                        <div class="price py-2">
-                            <span>$152</span>
-                        </div>
-                        <button type="submit" class="btn btn-warning font-size-12">Add to cart</button>
-                    </div>
-                </div>
-            </div>
-
-            <div class="item  py-2 bg-light">
-                <div class="product font-rale">
-                    <a href="#"><img src="./assests/products/3.png" alt="product1" class="img-fluid"></a>
-                    <div class="text-centure">
-                        <h6>Samsung Galaxy Tab</h6>
-                        <div class="rating text-warning font-size-12">
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="far fa-star"></i></span>
-                        </div>
-                        <div class="price py-2">
-                            <span>$152</span>
-                        </div>
-                        <button type="submit" class="btn btn-warning font-size-12">Add to cart</button>
-                    </div>
-                </div>
-            </div>
-
-            <div class="item  py-2 bg-light">
-                <div class="product font-rale">
-                    <a href="#"><img src="./assests/products/4.png" alt="product1" class="img-fluid"></a>
-                    <div class="text-centure">
-                        <h6>Samsung Galaxy Tab</h6>
-                        <div class="rating text-warning font-size-12">
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="far fa-star"></i></span>
-                        </div>
-                        <div class="price py-2">
-                            <span>$152</span>
-                        </div>
-                        <button type="submit" class="btn btn-warning font-size-12">Add to cart</button>
-                    </div>
-                </div>
-            </div>
-
-            <div class="item  py-2 bg-light">
-                <div class="product font-rale">
-                    <a href="#"><img src="./assests/products/5.png" alt="product1" class="img-fluid"></a>
-                    <div class="text-centure">
-                        <h6>Samsung Galaxy Tab</h6>
-                        <div class="rating text-warning font-size-12">
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="far fa-star"></i></span>
-                        </div>
-                        <div class="price py-2">
-                            <span>$152</span>
-                        </div>
-                        <button type="submit" class="btn btn-warning font-size-12">Add to cart</button>
-                    </div>
-                </div>
-            </div>
-
-            <div class="item  py-2 bg-light">
-                <div class="product font-rale">
-                    <a href="#"><img src="./assests/products/6.png" alt="product1" class="img-fluid"></a>
-                    <div class="text-centure">
-                        <h6>Samsung Galaxy Tab</h6>
-                        <div class="rating text-warning font-size-12">
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="far fa-star"></i></span>
-                        </div>
-                        <div class="price py-2">
-                            <span>$152</span>
-                        </div>
-                        <button type="submit" class="btn btn-warning font-size-12">Add to cart</button>
-                    </div>
-                </div>
-            </div>
+                <!--products-->
+                <?php
+            }
+            ?>
 
             <!--products-->
 
