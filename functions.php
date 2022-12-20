7f9eade327de6659182bf0f70a6083a1ccb9bd0f
Asel vinjitha algama <106462547+asel98x@users.noreply.github.com>
2022-12-20 17:44:24 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -4,7 +4,16 @@ use database\DBController;
 
 require ('database/DBController.php');
 require ('database/product.php');
+require ('database/Cart.php');
 
 $db = new DBController();
 
 $product = new product($db);
+
+$cart = new Cart($db);
+//$arr = array(
+//    "user_id"=>2,
+//    "item_id"=>9
+//);
+//
+//$cart->insertIntoCart($arr);
